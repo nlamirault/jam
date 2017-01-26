489f5a1272df8acd5449f7c8b76bd8788b6d7875
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2017-01-26 19:18:32 +0100

    Add CLI version
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- main.go
+++ main.go
@@ -1,12 +1,44 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/boltdb/bolt"
+
+	"github.com/budkin/jam/version"
 )
 
+const (
+	// BANNER is what is printed for help/info output.
+	BANNER = "Jam - %s\n"
+)
+
+var (
+	vers bool
+)
+
+func init() {
+	// parse flags
+	flag.BoolVar(&vers, "version", false, "print version and exit")
+
+	flag.Usage = func() {
+		fmt.Fprint(os.Stderr, fmt.Sprintf(BANNER, version.Version))
+		flag.PrintDefaults()
+	}
+
+	flag.Parse()
+
+	if vers {
+		fmt.Printf("%s\n", version.Version)
+		os.Exit(0)
+	}
+}
+
 func main() {
+
 	var err error
 	db, err = bolt.Open(fullDbPath(), 0600, nil)
 	checkErr(err)
